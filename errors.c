167841db3f5f6841cfb753227ed4b300928ea136
JaminBenjamins <jaminbenjamins@gmail.com>
2023-02-22 11:14:48 -0800

    function handling errors

--- errors.c
+++ errors.c
@@ -31,7 +31,7 @@ int _eputchar(char c)
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if ((c == BUF_FLUSH) || (i >= WRITE_BUF_SIZE))
 	{
 		write(2, buf, i);
 		i = 0;
